c71a8aa28510a28f37bec6ae3fca85f988e490eb
Coucou <iirthw@gmail.com>
2020-11-08 13:47:55 +0200

    wip on loader class

--- src/impl/yolo_obj_loader.cpp
+++ src/impl/yolo_obj_loader.cpp
@@ -1,1 +1,99 @@
 #include "yolo_obj_loader.h"
+
+#include <assert.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace yolo;
+
+ObjLoader::ObjLoader(const std::string& filePath) 
+: mFilePath(filePath)
+// TODO: implement caching of input data
+, mEnableCache(false)  {
+  assert(mFilePath.size());
+
+  auto inFileStream = std::ifstream(mFilePath);
+  if (inFileStream.is_open()) {
+    for (std::string nextLine; std::getline(inFileStream, nextLine);)
+      processLine(nextLine);
+  } else {
+    reportIOError();
+  }
+}
+
+/*===---------------------------------------------------------------------===*/
+
+std::vector<std::tuple<real_t, real_t, real_t>> ObjLoader::getVertexPositions() const {
+  // FIXME: return member var
+  auto vertexPositions = std::vector<std::tuple<real_t, real_t, real_t>>{};
+  return vertexPositions;
+}
+std::vector<std::tuple<int, int, int>> ObjLoader::getTriangles() const {
+  // FIXME: return member var  
+  auto triangles = std::vector<std::tuple<int, int, int>>{};
+  return triangles;
+}
+
+/*===---------------------------------------------------------------------===*/
+
+void ObjLoader::processLine(const std::string& line) {
+  if (line.empty())
+    return;
+
+  const auto firstCharOnLine = getFirstChar(line);
+  switch (firstCharOnLine) {
+    case 'v': {
+      readVertex(line);
+     break;
+    }
+    case 'f': {
+      readFace(line);
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+}
+
+void ObjLoader::readVertex(const std::string& line) {
+  std::istringstream inStrStream(line);
+  // FIXME: deal with literal issue float vs double
+  real_t vx{};
+  real_t vy{};
+  real_t vz = {};
+  if (!(inStrStream >> vx >> vy >> vz)) {
+    assert(false); 
+  }
+}
+
+void ObjLoader::readFace(const std::string& line) {
+  std::istringstream inStrStream(line);  
+  std::string vertId;
+  while (inStrStream >> vertId) {
+    //vertIds.push_back(std::stoi(vertId));
+  }
+
+  /*const auto nbVerts = vertIds.size();
+  if (nbVerts < 3 || nbVerts > 4) {
+    assert(false);
+    return;
+  } else {
+    for (int i = 0; i < 3; ++i)
+      
+  }*/
+}
+
+char ObjLoader::getFirstChar(const std::string& str) {
+  if (str.size() == 0)
+    assert(false);
+
+  return str.at(0);
+}
+
+void ObjLoader::reportIOError() const {
+  std::cerr << "Cannot open file " << mFilePath << std::endl;
+  assert(false);
+}
--- src/include/yolo_obj_loader.h
+++ src/include/yolo_obj_loader.h
@@ -1,1 +1,35 @@
 #pragma once
+
+#include <string>
+#include <tuple>
+#include <vector>
+
+#ifdef ENABLE_DOUBLE_PRECISION
+typedef double real_t;
+#else
+typedef float real_t;
+#endif
+
+namespace yolo
+{
+  class ObjLoader {
+  public:
+    ObjLoader(const std::string& filePath);
+
+    std::vector<std::tuple<real_t, real_t, real_t>> getVertexPositions() const;
+    std::vector<std::tuple<int, int, int>> getTriangles() const;
+
+  private:
+
+    void processLine(const std::string& line);
+    void readVertex(const std::string& line);
+    void readFace(const std::string& line);
+    static char getFirstChar(const std::string& str);
+
+    void reportIOError() const;
+    
+  private:
+    const bool mEnableCache;
+    const std::string mFilePath;
+  };
+} // namespace
